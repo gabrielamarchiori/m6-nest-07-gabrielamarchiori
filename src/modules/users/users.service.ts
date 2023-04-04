5eac556d8a962962c87b4fc29993220e3c2ed3d2
Gabriela <gabrielacamarchiori@gmail.com>
2023-04-04 09:06:15 -0300

    add primas

---
+++ src/modules/users/users.service.ts
@@ -0,0 +1,40 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UsersRepository } from './repositories/users.repository';
+
+@Injectable()
+export class UsersService {
+  constructor(private usersRepository: UsersRepository) {}
+  create(createUserDto: CreateUserDto) {
+    return this.usersRepository.create(createUserDto);
+  }
+
+  findAll() {
+    return this.usersRepository.findAll();
+  }
+
+  findOne(id: string) {
+    const findUser = this.usersRepository.findOne(id);
+    if (!findUser) {
+      throw new NotFoundException('user not found');
+    }
+    return findUser;
+  }
+
+  update(id: string, updateUserDto: UpdateUserDto) {
+    const findUser = this.usersRepository.findOne(id);
+    if (!findUser) {
+      throw new NotFoundException('user not found');
+    }
+    return this.usersRepository.update(id, updateUserDto);
+  }
+
+  remove(id: string) {
+    const findUser = this.usersRepository.findOne(id);
+    if (!findUser) {
+      throw new NotFoundException('user not found');
+    }
+    return this.usersRepository.delete(id);
+  }
+}
